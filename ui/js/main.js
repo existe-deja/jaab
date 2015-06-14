558f62da0333b19b336e4ebddae0a72d209a55b4
Pshit <arthur.chauffaille@gmail.com>
2015-06-14 21:28:29 +0200

    Menu style change scroll JS

--- ui/js/main.js
+++ ui/js/main.js
@@ -32,3 +32,62 @@ $(document).ready(function(){
     return false;
   });
 });
+
+$(document).ready(function() {
+        $('.scrollTo').click( function() { // Au clic sur un élément
+          var page = $(this).attr('href'); // Page cible
+          var speed = 750; // Durée de l'animation (en ms)
+          $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+          return false;
+        });
+      });
+
+$(document).ready(function(){      
+            $(window).resize(function() {
+               setTimeout( function(){
+                   window.location.href = window.location.href;
+               },1000); 
+            });
+            var scroll_pos = 0;
+            var positionElementInPage1 = $('#scroll1').offset().top;
+            var positionElementInPage2 = $('#scroll2').offset().top;
+            var positionElementInPage3 = $('#scroll3').offset().top;
+            var positionElementInPage4 = $('#scroll4').offset().top;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > positionElementInPage4-60) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#FFFFFF');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
+                    document.getElementById("jaab-logo").src = "ui/images/logo.png";
+                    $("#press").css('color', '#FFFFFF');
+                }
+                else if(scroll_pos > positionElementInPage3-60) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#3381f6');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
+                    document.getElementById("jaab-logo").src = "ui/images/logo2.png";
+                    $("#press").css('color', '#3381f6');
+                }
+                else if(scroll_pos > positionElementInPage2-60) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#FFFFFF');
+                    document.getElementById("jaab-logo").src = "ui/images/logo3.png";
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
+                    $("#press").css('color', '#FFFFFF');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
+                }
+                 else if(scroll_pos > positionElementInPage1-80) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#3381f6');
+                    document.getElementById("jaab-logo").src = "ui/images/logo2.png";
+                    $("#press").css('color', '#3381f6');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
+                }
+                 else {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#ffcb64');
+                    document.getElementById("jaab-logo").src = "ui/images/logo.png";
+                    $("#press").css('color', '#FFFFFF');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
+                }
+            });
+        });
+
+
+
