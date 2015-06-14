eaf9401b8b5ab84153862d5d03d6b3d76694cb5a
Pshit <arthur.chauffaille@gmail.com>
2015-06-14 21:42:47 +0200

    Bug btn scroll intro + ajustement scroll js dynamique

--- ui/js/main.js
+++ ui/js/main.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
         $('.scrollTo').click( function() { // Au clic sur un élément
           var page = $(this).attr('href'); // Page cible
           var speed = 750; // Durée de l'animation (en ms)
-          $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+          $('html, body').animate( { scrollTop: $(page).offset().top+55 }, speed ); // Go
           return false;
         });
       });
