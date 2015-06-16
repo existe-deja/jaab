20021ed492bfb8428d80e085997df738daae28c4
Pshit <arthur.chauffaille@gmail.com>
2015-06-16 21:38:51 +0200

    Modifications pour merle

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,5 +1,17 @@
+ /* TRACKING ANALYTICS */
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-64085792-1', 'auto');
+      ga('send', 'pageview');
+  /* FIN TRACKING ANALYTICS */
+
 $(document).ready(function(){
-	$('.subscribe-form').on('submit', function(e){
+
+  /* SUBSCRIBE */
+  $('.subscribe-form').on('submit', function(e){
     e.preventDefault();
     $form = $(this);
     $subscriber = $form.find("[name=subscriber]"); 
@@ -31,16 +43,84 @@ $(document).ready(function(){
 
     return false;
   });
-});
+  /* FIN SUBSCRIBE */
 
-$(document).ready(function() {
-    $('.scrollTo').click( function() { // Au clic sur un élément
+
+  /* SCROLL AUTO */
+  $('.scrollAction').click( function() { // Au clic sur un élément
       var page = $(this).attr('href'); // Page cible
       var speed = 750; // Durée de l'animation (en ms)
-      $('html, body').animate( { scrollTop: $(page).offset().top+55 }, speed ); // Go
-        return false;
+      $('html, body').animate( { 
+        scrollTop: $(page).offset().top+55 }, 
+        speed ); // Go
+      return false;
      });
-  });
+  /* FIN SCROLL AUTO */
+
+  /* CHANGEMENT STYLE MENU */
+  var scroll_pos = 0;
+  var positionElementInPage1 = $('#pannel-one').offset().top;
+  var positionElementInPage2 = $('#pannel-two').offset().top;
+  var positionElementInPage3 = $('#pannel-three').offset().top;
+  var positionElementInPage4 = $('#pannel-four').offset().top;
+
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > positionElementInPage4-60) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#FFFFFF');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
+                    $(".pure-menu-link[href='#beta-testeur']").css('transition', 'all 0.5s ease');
+                    $("#jaab-logo").attr('src','ui/images/logo-green.svg');
+                    $(".press").css('color', '#FFFFFF');
+                    $(".press").css('transition', 'all 0.5s ease');
+                    window.location.hash = 'decouvrez-comment-jaab-fonctionne';
+                }
+                else if(scroll_pos > positionElementInPage3-60) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#3381f6');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
+                    $("#jaab-logo").attr('src','ui/images/logo-blue.svg');
+                    $(".press").css('color', '#3381f6');
+                    window.location.hash = 'rejoignez-les-evenements-de-vos-amis';                  
+                }
+                else if(scroll_pos > positionElementInPage2-60) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#FFFFFF');
+                    $("#jaab-logo").attr('src','ui/images/logo-green.svg');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
+                    $(".press").css('color', '#FFFFFF');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
+                    window.location.hash = 'retrouvez-tous-vos-amis-simplement';
+                }
+                 else if(scroll_pos > positionElementInPage1-80) {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#3381f6');
+                    $("#jaab-logo").attr('src','ui/images/logo-blue.svg');
+                    $(".press").css('color', '#3381f6');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
+                    window.location.hash = 'tous-vos-evenements-au-meme-endroit';
+                }
+                 else {
+                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#ffcb64');
+                    $(".pure-menu-link[href='#beta-testeur']").css('transition', 'all 0.5s ease');
+                    $("#jaab-logo").attr('src','ui/images/logo-white.svg');
+                    $(".press").css('color', '#FFFFFF');
+                    $(".press").css('transition', 'all 0.5s ease');
+                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
+                    window.location.hash = 'intro';
+                }
+            });
+  /* CHANGEMENT STYLE MENU */
+
+  /* ANIMATION POPIN VIDEO */
+   $( ".openVid" ).click(function() {
+       $("#popPlayer").css('display', 'inline');
+    });
+
+   $( ".closeVid" ).click(function() {
+       $("#popPlayer").css('display', 'none');
+    });
+/* FIN ANIMATION POPIN VIDEO */
+
+});
+
 
 
 /* FUNCTION REFRESH */
@@ -52,63 +132,7 @@ $(document).ready(function() {
 });*/
 /* END FUNCTION REFRESH */
             
-            var scroll_pos = 0;
-            var positionElementInPage1 = $('#scroll1').offset().top;
-            var positionElementInPage2 = $('#scroll2').offset().top;
-            var positionElementInPage3 = $('#scroll3').offset().top;
-            var positionElementInPage4 = $('#scroll4').offset().top;
-
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > positionElementInPage4-60) {
-                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#FFFFFF');
-                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
-                    $(".pure-menu-link[href='#beta-testeur']").css('transition', 'all 0.5s ease');
-                    $("#jaab-logo").attr('src','ui/images/logo3.svg');
-                    $("#press").css('color', '#FFFFFF');
-                    $("#press").css('transition', 'all 0.5s ease');
-                    window.location.hash = 'decouvrez-comment-jaab-fonctionne';
-                }
-                else if(scroll_pos > positionElementInPage3-60) {
-                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#3381f6');
-                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
-                    $("#jaab-logo").attr('src','ui/images/logo2.svg');
-                    $("#press").css('color', '#3381f6');
-                    window.location.hash = 'rejoignez-les-evenements-de-vos-amis';                  
-                }
-                else if(scroll_pos > positionElementInPage2-60) {
-                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#FFFFFF');
-                    $("#jaab-logo").attr('src','ui/images/logo3.svg');
-                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
-                    $("#press").css('color', '#FFFFFF');
-                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#86c56c');
-                    window.location.hash = 'retrouvez-tous-vos-amis-simplement';
-                }
-                 else if(scroll_pos > positionElementInPage1-80) {
-                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#3381f6');
-                    $("#jaab-logo").attr('src','ui/images/logo2.svg');
-                    $("#press").css('color', '#3381f6');
-                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
-                    window.location.hash = 'tous-vos-evenements-au-meme-endroit';
-                }
-                 else {
-                    $(".pure-menu-link[href='#beta-testeur']").css('background-color', '#ffcb64');
-                    $(".pure-menu-link[href='#beta-testeur']").css('transition', 'all 0.5s ease');
-                    $("#jaab-logo").attr('src','ui/images/logo.svg');
-                    $("#press").css('color', '#FFFFFF');
-                    $("#press").css('transition', 'all 0.5s ease');
-                    $(".pure-menu-link[href='#beta-testeur']").css('color', '#FFFFFF');
-                    window.location.hash = 'intro';
-                }
-            });
+            
 
 
-/* ANIMATION POPIN VIDEO */
-   $( ".openVid" ).click(function() {
-       $("#popPlayer").css('display', 'inline');
-    });
-
-   $( ".closeVid" ).click(function() {
-       $("#popPlayer").css('display', 'none');
-    });
 
