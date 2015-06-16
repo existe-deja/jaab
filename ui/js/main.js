531190ba2e0f12d00f7b0d81b7725c8565424834
Pshit <arthur.chauffaille@gmail.com>
2015-06-17 00:05:30 +0200

    Fix bugs + pop-in video + animation JS + CSS

--- ui/js/main.js
+++ ui/js/main.js
@@ -121,31 +121,48 @@ $(document).ready(function(){
     });
   }
   });
-  /* CHANGEMENT STYLE MENU */
 
-  /* ANIMATION POPIN VIDEO */
-   $( ".openVid" ).click(function() {
-       $("#popPlayer").css('display', 'inline');
-    });
+var ripple_wrap = $('.ripple-wrap'),
+      rippler = $('.ripple'),
+      finish = false,
+      monitor = function(el) {
+        var computed = window.getComputedStyle(el, null),
+            borderwidth = parseFloat(computed.getPropertyValue('border-left-width'));
 
-   $( ".closeVid" ).click(function() {
-       $("#popPlayer").css('display', 'none');
-    });
-  /* FIN ANIMATION POPIN VIDEO */
+
+        if (!finish && borderwidth >= 1500) {
+          el.style.WebkitAnimationPlayState = "paused";
+          el.style.animationPlayState = "paused";
+        }
+        if (finish) {
+          el.style.WebkitAnimationPlayState = "running";
+          el.style.animationPlayState = "running";
+          return;
+        } else {
+          window.requestAnimationFrame(function() {monitor(el)});
+        }
+      };
+  
+  storedcontent = $('#content-2').html();
+  $('#content-2').remove();
+  
+  rippler.bind("webkitAnimationEnd oAnimationEnd msAnimationEnd mozAnimationEnd animationend", function(e){
+    ripple_wrap.removeClass('goripple');
+  });
+
+  $('body').on('click', ".openVid", function(e) {
+    rippler.css('left', e.clientX + 'px');
+    rippler.css('top', e.clientY + 'px');
+    e.preventDefault();
+    finish = false;
+    ripple_wrap.addClass('goripple');
+    $("#popPlayer").delay("slow").fadeIn("slow");
+    window.requestAnimationFrame(function() {monitor(rippler[0])});  
+  });
+  
+  $('body').on('click', ".closeVid", function(e) {
+    $("#popPlayer").delay("fast").fadeOut("fast");
+    finish = true;
+  });
+
 });
-
-
-
-/* FUNCTION REFRESH */
-/*$(document).ready(function(){      
-     $(window).resize(function() {
-       setTimeout( function(){
-       window.location.href = window.location.href;
-     },1000); 
-});*/
-/* END FUNCTION REFRESH */
-            
-            
-
-
-
