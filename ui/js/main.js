5a968304c014bb73ca3da3126219c57b36f4507e
Pshit <arthur.chauffaille@gmail.com>
2015-06-16 18:55:51 +0200

    Centrer logo + pop-in video test

--- ui/js/main.js
+++ ui/js/main.js
@@ -103,35 +103,12 @@ $(document).ready(function() {
             });
 
 
-/* FUNCTION AUTOSCROLL MOUSE-WHEEL */
-/*(function() {
-  var delay = false;
-
-  $(document).on('mousewheel DOMMouseScroll', function(event) {
-    event.preventDefault();
-    if(delay) return;
-
-    delay = true;
-    setTimeout(function(){delay = false},200)
-
-    var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-
-    var a= document.getElementsByTagName('a');
-    if(wd < 0) {
-      for(var i = 0 ; i < a.length ; i++) {
-        var t = a[i].getClientRects()[0].top;
-        if(t >= 40) break;
-      }
-    }
-    else {
-      for(var i = a.length-1 ; i >= 0 ; i--) {
-        var t = a[i].getClientRects()[0].top;
-        if(t < -20) break;
-      }
-    }
-    $('html,body').animate({
-      scrollTop: a[i].offsetTop
+/* ANIMATION POPIN VIDEO */
+   $( ".openVid" ).click(function() {
+       $("#popPlayer").css('display', 'inline');
     });
-  });
-})();*/
-/* END FUNCTION AUTOSCROLL MOUSE-WHEEL */
+
+   $( ".closeVid" ).click(function() {
+       $("#popPlayer").css('display', 'none');
+    });
+
